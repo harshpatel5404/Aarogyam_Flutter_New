80007471c9314d0dad5e713987575166438f7ed6
harshvora98 <harshad.vora@brainiuminfotech.com>
2022-12-10 11:15:03 +0530

    android v2 migration

--- android/app/src/main/kotlin/com/kendra/aarogyamswadeshi/MainActivity.kt
+++ android/app/src/main/kotlin/com/kendra/aarogyamswadeshi/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.kendra.aarogyamswadeshi
 
 import androidx.annotation.NonNull;
-import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
